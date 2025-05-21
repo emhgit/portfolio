06e3694434c78d07ce724c2f5b5a92c2b0631f82
emhgit <eharper0815@gmail.com>
2025-05-20 19:23:48 -0500

    add css to app page

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,17 @@ function App() {
     <>
       <NavBar />
 
-      <div class="image-container">
-        <img src="src\assets\image-1.jpg" alt="image-1" width="200"  />
-      </div>
+      <main>
+        <div id="image-container">
+          <img src="src\assets\image-1.jpg" alt="image-1" width="200"  />
+        </div>
 
-      <main id="hero">
-        <h1>Elliott Harper</h1>
-        <h2>Full-Stack Developer</h2>
-        <div id="socials-container"></div>
-        <button>About Me</button>
+        <section id="hero">
+          <h1>Elliott Harper</h1>
+          <h2>Full-Stack Developer</h2>
+          <div id="socials-container"></div>
+          <button>About Me</button>
+        </section>
       </main>
       
     </>
