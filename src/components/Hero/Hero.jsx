e127635bd1c6a84da4d28fc8696c17f52f97ab58
emhgit <eharper0815@gmail.com>
2025-05-21 22:42:43 -0500

    change text card bg color

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -15,14 +15,12 @@ const Hero = () => {
         <section id={styles["hero"]}>
             <h1>Elliott Harper</h1>
             <h2>Full-Stack Developer</h2>
-            <div id="socials-container"></div>
-            <button><Link to="/about-me" className={styles["nav-link"]}>About Me</Link></button>
             <section id={styles["socials-container"]}>
               <a href="https://github.com/emhgit"><img src={theme === 'light' ? "/src/assets/github-logo.png" : "/src/assets/light-github-logo.png"} alt="github logo" /></a>
               <a href="https://leetcode.com/u/emhleet/"><img src="/src/assets/leetcode-logo.png" alt="leetcode logo" /></a>
-              
-              <img src={theme === 'light' ? "/src/assets/dark-hackerrank-logo.png" : "/src/assets/hackerrank-logo.png"} alt="hackerrank logo" />
-          </section>
+              <a href="https://www.hackerrank.com/profile/emhhacker"><img src={theme === 'light' ? "/src/assets/dark-hackerrank-logo.png" : "/src/assets/hackerrank-logo.png"} alt="hackerrank logo" /></a>
+            </section>
+            <button><Link to="/about-me" className={styles["nav-link"]}>About Me</Link></button>
         </section>
 
     </main>
