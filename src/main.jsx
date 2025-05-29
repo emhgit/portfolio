820698895dcd67b4bb2fde73082b2624af24aed7
emhgit <eharper0815@gmail.com>
2025-05-29 15:54:58 -0500

    update package.json for deployment and update browserrouter

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/<repo-name>">
         <App />
       </BrowserRouter>
     </ThemeProvider>
