7fea48fe0117577316ece77891207a80ffdf5f41
emhgit <eharper0815@gmail.com>
2025-07-02 20:52:04 -0500

    add usaco guide to projects section

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,26 +1,42 @@
-import React from 'react';
+import React from "react";
 import styles from "./ProjectCard.module.css";
 
 const ProjectCard = (props) => {
-    const title = props.title;
-    const src = props.src;
-    const description = props.description;
-    const githubLink = props["github-link"];
+  const title = props.title;
+  const src = props.src;
+  const description = props.description;
+  const githubLink = props["github-link"];
+  const thumbnail = props.thumbnail;
+  const websiteLink = props["website-link"];
 
-    return (
-        <div className={styles["project-card"]}>
-            <h2>{title}</h2>
-            <iframe
-                src={src}
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-            ></iframe>
-            <p>{description}</p>
-            <a href={githubLink}>View Project Repository Here</a>
-        </div>
-    )
-}
+  return (
+    <div className={styles["project-card"]}>
+      <h2>{title}</h2>
+      {thumbnail && (
+        <img
+          src={thumbnail}
+          alt={`${title} thumbnail`}
+          className={styles["project-thumbnail"]}
+        />
+      )}
+      {src && (
+        <iframe
+          src={src}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      )}
+      <p>{description}</p>
+      <a href={githubLink}>View Project Repository Here</a>
+      {websiteLink && (
+        <a href={websiteLink} target="_blank" rel="noopener noreferrer">
+          View Live Project Here
+        </a>
+      )}
+    </div>
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
--- src/pages/Experience/Experience.jsx
+++ src/pages/Experience/Experience.jsx
@@ -9,6 +9,13 @@ const Experience = () => {
 
       <div id={styles["projects-container"]}>
         <ProjectCard
+          title="USACO Guide"
+          description="USACO Guide is a comprehensive resource for students preparing for the USA Computing Olympiad. I contributed UI and content modules to the website."
+          github-link="https://github.com/cpinitiative/usaco-guide"
+          thumbnail="public/assets/usaco-guide.jpg"
+          website-link="https://usaco.guide/"
+        />
+        <ProjectCard
           title="Economic Data Visualizer"
           src="https://www.youtube.com/embed/pk1vj2S6Z0Y?si=TZPEmcQy76lWB_7U"
           description="Economic Data Visualizer is a web application that fetches real-time economic indicators from the 
