5eb06df5bd6ef0d70e53d39f3b6fb487ed26d758
emhgit <eharper0815@gmail.com>
2025-07-31 17:53:46 -0500

    remove frameBorder property from iframe in ProjectCard bc its deprecated

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -23,7 +23,6 @@ const ProjectCard = (props) => {
         <iframe
           src={src}
           title="YouTube video player"
-          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
